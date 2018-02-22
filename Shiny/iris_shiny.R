36ba1b71904d2f2ca1a37214c84673ffa101a1a9
katiebalcewicz <kh.balcewicz@gmail.com>
2018-02-22 09:18:59 -0500

    Added test shiny app

---
+++ Shiny/iris_shiny.R
@@ -0,0 +1,34 @@
+#iris_shiny.r
+
+server = function(input, output, session) {
+  
+  # Combine the selected variables into a new data frame
+  selectedData <- reactive({
+    iris[, c(input$xcol, input$ycol)]
+  })
+  
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+  
+  output$table <- renderDataTable(
+    cbind(selectedData(), data.frame(clusters()$cluster)),
+    options = list(pageLength = 15)
+  )
+}
+
+ui = pageWithSidebar(
+  headerPanel('Iris k-means clustering'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(iris)),
+    selectInput('ycol', 'Y Variable', names(iris),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 9)
+  ),
+  mainPanel(
+    dataTableOutput('table')
+  )
+)
+
+shinyApp(ui, server)
